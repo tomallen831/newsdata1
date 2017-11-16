cf6bc84b5ed265d5af74f3288f990929489aa30e
Tom Allen <tallen831@gmail.com>
2017-11-16 12:26:27 -0500

    Fixed styleguide issues

--- news1.py
+++ news1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 import psycopg2
 DB_NAME = "news"
 
@@ -12,27 +13,30 @@ def connect_db(question):
     db.close()
     return results
 
-#1.What are the most popular three articles of all time?
+# 1. What are the most popular three articles of all time?
 question_1 = """select title, views
                     from popular_articles_view limit 3"""
 
-#2.Who are the most popular article authors of all time?
+
+# 2. Who are the most popular article authors of all time?
 question_2 = """select authors.name, sum(popular_articles_view.views) as views
                     from popular_articles_view, authors
                     where authors.id = popular_articles_view.author
                     group by authors.name
                     order by views desc"""
 
-#3.On which days did more than 1% of requests lead to errors?
+
+# 3. On which days did more than 1% of requests lead to errors?
 question_3 = """select * from error_days_view
                     where \"Error Percentage\" > 1"""
 
+
 # Print questions
 def print_question(title):
-    print ("\n" + title + "\n")
+    print("\n" + title + "\n")
 
 
-# 2.Who are the most popular article authors of all time?
+# Print 1. Who are the most popular article authors of all time?
 def popular_articles():
     popular_articles = connect_db(question_1)
     print_question("1. What are the most popular three articles of all time?")
@@ -41,7 +45,7 @@ def popular_articles():
         print(" \"{}\" -- {} views".format(title, num))
 
 
-# Print 3.On which days did more than 1% of requests lead to errors?
+# Print 2. On which days did more than 1% of requests lead to errors?
 def popular_authors():
     popular_authors = connect_db(question_2)
     print_question("2. Who are the most popular article authors of all time?")
@@ -50,13 +54,15 @@ def popular_authors():
         print(" {} -- {} views".format(name, num))
 
 
-# Print the days in which there were more than 1% bad requests
+# Print 3. On which days did more than 1% of requsets lead to errors?
 def error_days():
     error_days = connect_db(question_3)
-    print_question("3. On which days did more than 1% of requests lead to errors?")
+    print_question("3. On which days did more than
+    1% of requests lead to errors?")
 
     for day, errorpercentage in error_days:
-        print("""{0:%B %d, %Y} -- {1:.2f} % errors""".format(day, errorpercentage))
+        print("""{0:%B %d, %Y} -- {1:.2f} % errors""".
+        format(day, errorpercentage))
 
 
 if __name__ == '__main__':
